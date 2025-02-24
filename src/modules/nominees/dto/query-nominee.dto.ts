b0a69e54b02783aa1b5d9247614214a67c27c208
RichardVigh <killuaz1739@gmail.com>
2025-02-24 20:04:40 +0100

    Closes #4

--- src/modules/nominees/dto/query-nominee.dto.ts
+++ src/modules/nominees/dto/query-nominee.dto.ts
@@ -1,6 +1,7 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { QueryDto } from '../../../common/dto/query.dto';
+import { Transform } from 'class-transformer';
 
 export class NomineeQueryDto extends QueryDto {
   @ApiPropertyOptional({
@@ -10,4 +11,13 @@ export class NomineeQueryDto extends QueryDto {
   @IsOptional()
   @IsString()
   title?: string;
+
+  @ApiPropertyOptional({
+    description: 'Filter by winner',
+    example: true,
+  })
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }) => value === 'true' || value === true) // Converts string "true"/"false" to boolean
+  winner?: boolean;
 }
